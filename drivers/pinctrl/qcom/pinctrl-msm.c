pocox3pro/android_kernel_xiaomi_sm8150

drivers: pinctrl: Import Xiaomi changes
Change-Id: I25b44580b4a55926dd2ef3148f682228f18a8c9f

drivers/pinctrl/qcom/pinctrl-msm.c
@@ -300,6 +300,12 @@ static int msm_config_group_get(struct pinctrl_dev *pctldev,
 	int ret;
 	u32 val;
 
+#ifdef CONFIG_MACH_XIAOMI_SM8250
+	/* gpio 0~3 is NFC spi, gpio 126~129 is FP spi */
+	if (group < 4 || (group > 125 && group < 130))
+		return 0;
+#endif
+
 	g = &pctrl->soc->groups[group];
 	base = reassign_pctrl_reg(pctrl->soc, group);
 
@@ -622,6 +628,12 @@ static void msm_gpio_dbg_show(struct seq_file *s, struct gpio_chip *chip)
 	unsigned i;
 
 	for (i = 0; i < chip->ngpio; i++, gpio++) {
+#ifdef CONFIG_MACH_XIAOMI_SM8250
+		/* gpio 0~3 is NFC spi, gpio 126~129 is FP spi */
+		if (i < 4 || (i > 125 && i < 130))
+			continue;
+#endif
+
 		msm_gpio_dbg_show_one(s, NULL, chip, i, gpio);
 		seq_puts(s, "\n");
 	}

Dark-Matter7232/android_kernel_xiaomi_nabu

drivers/pinctrl/qcom/pinctrl-msm.c
@@ -300,6 +300,12 @@ static int msm_config_group_get(struct pinctrl_dev *pctldev,
 	int ret;
 	u32 val;
 
+#ifdef CONFIG_MACH_XIAOMI_SM8150
+	/* gpio 0~3 is NFC spi, gpio 126~129 is FP spi */
+	if (group < 4 || (group > 125 && group < 130))
+		return 0;
+#endif
+
 	g = &pctrl->soc->groups[group];
 	base = reassign_pctrl_reg(pctrl->soc, group);
 
@@ -622,6 +628,12 @@ static void msm_gpio_dbg_show(struct seq_file *s, struct gpio_chip *chip)
 	unsigned i;
 
 	for (i = 0; i < chip->ngpio; i++, gpio++) {
+#ifdef CONFIG_MACH_XIAOMI_SM8150
+		/* gpio 0~3 is NFC spi, gpio 126~129 is FP spi */
+		if (i < 4 || (i > 125 && i < 130))
+			continue;
+#endif
+
 		msm_gpio_dbg_show_one(s, NULL, chip, i, gpio);
 		seq_puts(s, "\n");
 	}