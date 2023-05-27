b52b21c8fae5aecc3fb4bb879e9a0d459e5aced9
Dark-Matter7232 <me@const.eu.org>
2023-05-27 19:59:56 +0530

    treewide: nuke logspam
    - makes kernel log more readable.
    
    Signed-off-by: Dark-Matter7232 <me@const.eu.org>

--- drivers/power/supply/qcom/step-chg-jeita-nabu.c
+++ drivers/power/supply/qcom/step-chg-jeita-nabu.c
@@ -106,6 +106,23 @@ static struct step_chg_info *the_chip;
 #define GET_CONFIG_RETRY_COUNT		50
 #define WAIT_BATT_ID_READY_MS		200
 
+#undef dev_info
+#define dev_info(x, ...)
+#undef dev_dbg
+#define dev_dbg(x, ...)
+#undef dev_err
+#define dev_err(x, ...)
+#undef pr_info
+#define pr_info(x, ...)
+#undef pr_debug
+#define pr_debug(x, ...)
+#undef pr_error
+#define pr_error(x, ...)
+#undef printk
+#define printk(x, ...)
+#undef printk_deferred
+#define printk_deferred(x, ...)
+
 static bool is_batt_available(struct step_chg_info *chip)
 {
 	if (!chip->batt_psy)
